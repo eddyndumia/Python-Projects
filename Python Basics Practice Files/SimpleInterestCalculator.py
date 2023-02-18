a3a83149a12059cb29926fa3082f63e3655915a5
Eddy Ndumia <ndumiaeddy@gmail.com>
2023-02-18 12:21:43 +0300

    initial commit

---
+++ Python Basics Practice Files/SimpleInterestCalculator.py
@@ -0,0 +1,12 @@
+
+# Demonstration of Float Number
+
+# Simple Interest Calculator 
+
+princ = float(input("Enter the Principle Amount:"))
+rate = float(input("Enter Rate of Interest:"))
+time = float(input("Enter Time Period:"))
+
+si = (princ * rate * time)/100
+print("Simple Interest=", si)
+
---
+++ Python Basics Practice Files/pythonStart.py
@@ -0,0 +1,13 @@
+
+
+
+number  = int(input("Enter your number here: "))
+
+print("now rebooting to add your second number...this might take some years!")
+
+print("....")
+
+number2 = int(input("...alright. I think i'm done. Enter your second number:"))
+
+print(number+number2)
+
